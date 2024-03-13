15865b1988505ec6caafea2f74c37ffb0cb1c13d
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-03-13 18:18:20 +0000

    fix: knip

--- knip.ts
+++ knip.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: ["static/scripts/onboarding/onboarding.ts", "static/scripts/rewards/index.ts"],
+  entry: ["static/scripts/onboarding/onboarding.ts"],
   project: ["static/**/*.ts"],
   ignoreExportsUsedInFile: true,
   ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier", "@octokit/core"],
