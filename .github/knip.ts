23f3344cc1272cab297a15d952ee8db90060be0d
zugdev <zug.dev@proton.me>
2024-12-26 04:45:06 -0300

    chore: fix knip

--- .github/knip.ts
+++ .github/knip.ts
@@ -6,7 +6,18 @@ const config: KnipConfig = {
   ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
-  ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier", "@types/jest", "@mswjs/data"],
+  ignoreDependencies: [
+    "eslint-config-prettier",
+    "eslint-plugin-prettier",
+    "@types/jest",
+    "@mswjs/data",
+    "@coinbase/wallet-sdk",
+    "@reown/appkit",
+    "@reown/appkit-adapter-ethers5",
+    "ethers",
+    "@types/react",
+    "react",
+  ],
   eslint: true,
 };
 
